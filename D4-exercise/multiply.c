e4cdd42e5f0498ba3d80d68c5b0e4d56813b4608
pierluigi.dicerbo@gmail.com <pdicerbo@login1.(none)>
2015-10-19 14:56:43 +0200

    def version

--- D4-exercise/multiply.c
+++ D4-exercise/multiply.c
@@ -6,24 +6,30 @@
 
 void print_lines(double*, int, int);
 void std_out_print(double*, int, int, int, int);
+void partial_prod(double*, double*, double*, int, int);
 
 int main(int argc, char** argv){
 
   int MyID, NPE, i, j, i_global, tmp_idx;
-  int block, left, right;
+  int block, prev, next;
 
-  int n_iter = 0;
-  int i_in, j_in, k_in, offset, A_offset; /* inner index */
+  int shift;
+  int tag = 42;
 
-  double *A, *B, *C;
+  double *A, *B, *C, *B_rec;
+
+  MPI_Request first_request, request = MPI_REQUEST_NULL;
+  MPI_Status status;
 
   MPI_Init(&argc, &argv);
 
   MPI_Comm_rank(MPI_COMM_WORLD, &MyID);
   MPI_Comm_size(MPI_COMM_WORLD, &NPE);
 
-  right = (MyID + 1) % NPE;
-  left = (MyID + NPE - 1) % NPE;
+  next = (MyID + 1) % NPE;
+  prev = (MyID + NPE - 1) % NPE;
+
+  shift = MyID;
 
   if(NPE > SIZE){
     printf("\n\tThe number of processes is bigger then the size of the problem\n");
@@ -39,56 +45,81 @@ int main(int argc, char** argv){
   A = (double*)malloc(SIZE * block * sizeof(double));
   B = (double*)malloc(SIZE * block * sizeof(double));
   C = (double*)malloc(SIZE * block * sizeof(double));
-
+  
   for(i = 0; i < block; i++){
 
     tmp_idx = i*SIZE;
     i_global = block * MyID + i;
 
     for(j = 0; j < SIZE; j++){
-      A[j + tmp_idx] = i_global - j;
+      /* A[j + tmp_idx] = i_global - j; */
+      A[j + tmp_idx] = i_global * SIZE + j + 1;
+      B[j + tmp_idx] = i_global * SIZE + j + 1;
       C[j + tmp_idx] = 0.;
 
       /* B is the identity matrix */
-      if(j == i_global){
-	A[j + tmp_idx] = 1.;
-	B[j + tmp_idx] = 1.;
-      }
-      else{
-	A[j + tmp_idx] = 0.;
-	B[j + tmp_idx] = 0.;
-      }
+      /* if(j == i_global){ */
+      /* 	/\* A[j + tmp_idx] = 1.; *\/ */
+      /* 	B[j + tmp_idx] = 1.; */
+      /* } */
+      /* else{ */
+      /* 	/\* A[j + tmp_idx] = 0.; *\/ */
+      /* 	B[j + tmp_idx] = 0.; */
+      /* } */
     }
   }
 
+  partial_prod(A, B, C, block, shift);
 
-  /* loop over block */
+  B_rec = (double*)malloc(SIZE * block * sizeof(double));
+
+  MPI_Isend(B, SIZE * block, MPI_DOUBLE, prev, tag, MPI_COMM_WORLD, &first_request);
+
+  for(i = 1; i < NPE; i++){
+    MPI_Wait(&request, &status);
+    MPI_Recv(B_rec, SIZE * block, MPI_DOUBLE, next, tag, MPI_COMM_WORLD, &status);
+
+    if(i < NPE - 1)
+      MPI_Isend(B_rec, SIZE * block, MPI_DOUBLE, prev, tag, MPI_COMM_WORLD, &request);
+
+    if(i == 1)
+      MPI_Wait(&first_request, &status);
+
+    shift = (shift + 1) % NPE;
+
+    partial_prod(A, B_rec, C, block, shift);
+    
+  }
+
+  /* Printing result */
+  /* The 0. stay for "rest" in previous std_out_print version*/
+  std_out_print(A, MyID, block, 0., NPE); 
+  std_out_print(B, MyID, block, 0., NPE);
+  std_out_print(C, MyID, block, 0., NPE);
+
+  MPI_Finalize();
+
+  return 0;
+}
+
+void partial_prod(double* A, double* B, double* C, int block, int shift){
+
+  int j, i_in, j_in, k_in, offset, A_offset, tmp_idx; /* inner-block index */
+
+  /* loop over blocks */
   for(j = 0; j < SIZE; j += block){
-
-    offset = j; // * block;
 
     /* single block loop */
     for(i_in = 0; i_in < block; i_in++){
 
       tmp_idx = i_in * SIZE;
-      A_offset = MyID * block;
+      A_offset = shift * block;
 
-      for(j_in = offset; j_in < offset + block; j_in++){
+      for(j_in = j; j_in < j + block; j_in++)
 	for(k_in = 0; k_in < block; k_in++)
 	  C[j_in + tmp_idx] += A[k_in + tmp_idx + A_offset] * B[j_in + k_in*SIZE];
-      }
     }
-  }
-
-  std_out_print(A, MyID, block, 0., NPE); /* The 0. stay for "rest" in previous std_out_print version*/
-
-  std_out_print(B, MyID, block, 0., NPE); /* The 0. stay for "rest" in previous std_out_print version*/
-
-  std_out_print(C, MyID, block, 0., NPE); /* The 0. stay for "rest" in previous std_out_print version*/
-
-  MPI_Finalize();
-
-  return 0;
+  }  
 }
 
 void std_out_print(double* tmp_buf, int MyRank, int block, int rest, int NPE){
