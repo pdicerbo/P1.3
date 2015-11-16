1e3ff984038818540381ac5794254e5317e0a5bf
pierluigi.dicerbo@gmail.com <pdicerbo@login1.(none)>
2015-11-16 17:41:46 +0100

    commit

--- D7-exercise/hello_world.c
+++ D7-exercise/hello_world.c
@@ -3,13 +3,16 @@
 #include <omp.h>
 
 int main(int argc, char** argv){
-
   printf("\n\n\tSequential part...\n");
+#ifdef LINK_OMP
 #pragma omp parallel
 {
-  printf("\thello from thread number %d\n", omp_get_thread_num());
-
+  printf("\thello from thread number %d;\n", omp_get_thread_num());
 }
+#else
+  printf("\thello from thread number 0\n");
+#endif /* LINK_OMP */
+
   printf("\n\tBack to sequential part...\n\n");
 
   return 0;
