3d0f0c3601bba9853fcc87b7b135234c279169e4
pdicerbo <pierluigi.dicerbo@gmail.com>
2015-11-20 16:19:11 +0100

    add plot timing

--- D6-assignment/last_plot.py
+++ D6-assignment/last_plot.py
@@ -18,7 +18,28 @@ speed2 = np.array([t1[0] / t3[i] for i in range(len(t3))])
 speed3 = np.array([t1[0] / t4[i] for i in range(len(t4))])
 
 print(th)
-print(speed)
+print(t1)
+print(t2)
+
+n_mes = 6
+bar_width = .2
+index = np.arange(1, n_mes + 1);
+print(index)
+
+plt.figure()
+
+plt.bar(index, t1, bar_width, label = 'OMP_NUM_THREADS = 0')
+plt.bar(index + bar_width, t2, bar_width, color = 'r', label = 'OMP_NUM_THREADS = 2')
+plt.bar(index + 2 * bar_width, t3, bar_width, color = 'g', label = 'OMP_NUM_THREADS = 5')
+plt.bar(index + 3 * bar_width, t4, bar_width, color = 'c', label = 'OMP_NUM_THREADS = 10')
+
+plt.title("MPI vs OpenMP timing")
+plt.xlabel("Number of Nodes")
+plt.ylabel("Time (s)")
+plt.xticks(index + 2 * bar_width, index)
+plt.legend()
+plt.savefig("mpi_vs_omp_timing.png")
+plt.close('all')
 
 plt.figure()
 plt.plot(th, speed,  '-o', label = 'OMP_NUM_THREADS = 0')
