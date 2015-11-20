d059c3a24c00beb0fb4f0894fe712fccf3d05bd2
pierluigi.dicerbo@gmail.com <pdicerbo@login1.(none)>
2015-11-20 15:26:09 +0100

    last commit

--- D7-exercise/CUDA/cuda_stuff.cu
+++ D7-exercise/CUDA/cuda_stuff.cu
@@ -21,6 +21,22 @@ __global__ void transpose(double* mat, double* res, int ncol){
   res[c * ncol + r] = mat[index];
 }
 
+__global__ void mm_mult(double* A, double* B, double* C, int nr_A, int nc_A, int nc_B){
+
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  
+  // global index associated to matrix
+  int r = index / nc_B;
+  int c = index % nc_B;
+  int k;
+  double tmp = 0.;
+
+  for(k = 0; k < nc_A; k++)
+    tmp += A[k + r * nc_A] * B[c + k * nc_A];
+
+  C[index] = tmp;
+}
+
 int main(int argc, char** argv){
 
   int i, j, i_tmp;
@@ -29,6 +45,8 @@ int main(int argc, char** argv){
 
   double* mat = (double*)malloc(N * N * sizeof(double));
   double *dev_mat, *res_mat;
+  double *identity = (double*)malloc(N * N * sizeof(double));
+  double *dev_id, *dev_prod;
 
   if(THREADS * BLOCKS != N){
     printf("\n\tTHREADS * BLOCK must be equal to N\n\tExit\n");
@@ -57,9 +75,16 @@ int main(int argc, char** argv){
 
   printf("\n\tTRANSPOSE\n\n");
 
-  for(i = 0; i < N; i++)
-    for(j = 0; j < N; j++)
-      mat[j + i*N] = j + i*N;
+  for(i = 0; i < N; i++){
+    i_tmp = i * N;
+    for(j = 0; j < N; j++){
+      mat[j + i_tmp] = j + i_tmp;
+      if(i != j)
+	identity[i_tmp + j] = 0.;
+      else
+	identity[i_tmp + j] = 1.;
+    }
+  }
 
   for(i = 0; i < N; i++){
     i_tmp = i * N;
@@ -72,23 +97,26 @@ int main(int argc, char** argv){
 
   cudaMalloc(&dev_mat, N * N * sizeof(double));
   cudaMalloc(&res_mat, N * N * sizeof(double));
+  cudaMalloc(&dev_id,  N * N * sizeof(double));
+  cudaMalloc(&dev_prod,N * N * sizeof(double));
 
   cudaMemcpy(dev_mat, mat, N * N * sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_id, identity, N * N * sizeof(double), cudaMemcpyHostToDevice);
 
-  // transpose<<<THREADS, BLOCKS>>>(dev_mat, mat, N);
   transpose<<<THREADS, THREADS>>>(dev_mat, res_mat, N);
+  mm_mult<<<THREADS, THREADS>>>(res_mat, dev_id, dev_prod, N, N, N);
 
-  cudaMemcpy(mat, res_mat, N * N * sizeof(double), cudaMemcpyDeviceToHost);
+  cudaMemcpy(mat, dev_prod, N * N * sizeof(double), cudaMemcpyDeviceToHost);
   printf("\n\n");
   for(i = 0; i < N; i++){
+
     i_tmp = i * N;
 
     for(j = 0; j < N; j++)
       printf("\t%lg", mat[i_tmp + j]);
-
     printf("\n");
-}
-  
+  }
+  printf("\n");
 
   return 0;
 }
